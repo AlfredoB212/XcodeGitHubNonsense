48af26194b984bd363a4cd529acc1f1c48d47157
AlfredoB212 <alfredobarragan@pursuits-macbook-pro-93.local>
2018-10-05 14:34:45 -0400

    Initial Commit

---
+++ XcodeGitHub/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeGitHub
+//
+//  Created by Alfredo Barragan on 10/5/18.
+//  Copyright © 2018 Alfredo Barragan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
